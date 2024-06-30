d62b35631619033db5acc2751a2f3e38f8bfe7f1
Tiago Santos <tiagocsantos07@gmail.com>
2024-06-30 17:20:07 +0100

    final commit

--- Exercicio 6.R
+++ Exercicio 6.R
@@ -22,3 +22,22 @@ prob_exata <- 1- pgamma(126, 40, rate = 1/4)
 
 difference <- abs(proportion - prob_exata) * 100
 round(difference, 4)
+
+
+seed <- 1973
+n <- 40
+a <- 4
+valor <- 126
+
+set.seed(seed)
+y <- vector(length = 1000)
+
+for (i in 1:1000) {
+  y[i] <- sum(rexp(n, rate = 1 / a))
+}
+sim <- sum(y > valor) / 1000
+
+ext <- 1 - pgamma(valor, n, rate = 1 / a)
+
+solution <- abs(sim - ext) * 100
+solution
--- Exercicio 7.R
+++ Exercicio 7.R
@@ -27,3 +27,33 @@ quantil_verdadeiro <- 4.5/(0.75^(1/theta))
 
 desvio_absoluto <- abs(quantil_estimado - quantil_verdadeiro)
 round(desvio_absoluto, 4)
+
+
+data <- c(8.54,4.76,5.15,4.96,6.25,7.22,12.9,6.04,8.86,4.88,6.54,4.53,4.7,5.38,5.96,5.17,5.09,5.11)
+valor_inicial <- 3.4
+p <- 0.25
+theta <- 3.4
+a <- 4.5
+
+library(stats4)
+
+soma_logdata <- sum(log(data))
+n <- length(data)
+
+fun <- function(theta) {
+  res <- -n * log(theta) + (theta + 1) * soma_logdata - n * theta * log(a)
+  return(res)
+}
+
+result <- mle(minuslog = fun, start = list(theta = valor_inicial))
+est_theta <- as.vector(result@coef)
+
+quantil <- function(p, theta) {
+  res <- a * (1 - p)^(-1 / theta)
+}
+
+estimativa <- quantil(p, est_theta)
+valor_exato <- quantil(p, theta)
+
+solution <- abs(estimativa - valor_exato)
+solution
